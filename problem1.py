a5c9ab9a7e4e4cd16ed852db56f71dc4d60a59c3
FaridaNarimanova <farida.narimanova25@ycdsbk12.ca>
2024-02-24 00:22:10 -0500

    kPa is defined

---
+++ problem1.py
@@ -0,0 +1,9 @@
+"""
+Given the temperature at which water begins to boil,
+ determine atmospheric pressure.
+ 
+"""
+
+tempereture = 102
+atmospheric_pressure = 5 * tempereture - 400
+print("atmospheric pressure is " + str(atmospheric_pressure) +" kPa")
